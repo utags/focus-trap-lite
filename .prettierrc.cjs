9a499fee3aa6be0427189f20d502447e65c6d045
pipecraft <pipecraft.net@gmail.com>
2026-01-05 01:38:01 +0800

    chore: update prettierrc

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -10,4 +10,19 @@ module.exports = {
   trailingComma: 'es5',
   bracketSpacing: true,
   bracketSameLine: true,
+  overrides: [
+    {
+      files: ['*.css', '*.scss', '*.yml', 'build/**/*'],
+      options: {
+        singleQuote: false,
+      },
+    },
+    {
+      files: 'src/messages/*.ts',
+      options: {
+        printWidth: 9999,
+      },
+    },
+    { files: '*.json', options: { parser: 'json-stringify' } },
+  ],
 }
